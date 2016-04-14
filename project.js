2f626b3505ef118ec8aaa9e22f6691b6ad2bc09b
Jonathan Richardson <jrichy@sbcglobal.net>
2016-04-14 12:01:59 -0700

    fixed dropdown toggle

--- project.js
+++ project.js
@@ -5,19 +5,19 @@ $(document).ready(function(){
       $("#dropdown1 li").show();
       $(this).closest('.btn-group').find(".btn:first-child").text($(this).text());
       $(this).closest('.btn-group').find(".btn:first-child").val($(this).text());
-      $("#dropdown1 li").hide();
+      $('#dropdown1 li').dropdown('toggle');
     });
 
     $("#dropdown2 li").click(function(){
       $(this).closest('.btn-group').find(".btn:first-child").text($(this).text());
       $(this).closest('.btn-group').find(".btn:first-child").val($(this).text());
-      $("#dropdown2 li").toggle();
+      $('#dropdown1 li').dropdown('toggle');
     });
 
     $("#dropdown3 li").click(function(){
       $(this).closest('.btn-group').find(".btn:first-child").text($(this).text());
       $(this).closest('.btn-group').find(".btn:first-child").val($(this).text());
-      $("#dropdown3 li").toggle();
+      $('#dropdown3 li').dropdown('toggle');
     });
   });
 
