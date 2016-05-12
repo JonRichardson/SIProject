0483d8ae9f660e83002b3efa285e7e107d4a2372
Ramin Mesghali <rmesghali2004@yahoo.com>
2016-05-12 09:20:11 -0700

    added comments

--- js/project.js
+++ js/project.js
@@ -35,36 +35,4 @@ $(document).ready(function(){
   $("#userInput3").click(function(){
     $(".newChevron3").hide("hiddenChevron");
   });
-
-  // Function for the doughnut chart on the index2 page
-  $(function () {
-    var ctx = $("#myChart").get(0).getContext("2d");
-  	var data = [
-  		{
-  			value: 170,
-  			color: "#229B47",
-  			highlight: "#4D6E43",
-  			label: "Tin"
-  		},
-  		{
-  			value: 80,
-  			color: "#E9D620",
-  			highlight: "#EBB71E",
-  			label: "Tungsten"
-  		},
-      {
-  			value: 100,
-  			color: "#11B6D0",
-  			highlight: "#53C7DE",
-  			label: "Tantalum"
-  		},
-  		{
-  			value: 40,
-  			color: "#F79123",
-  			highlight: "#F26621",
-  			label: "Gold"
-  		}
-  	];
-	  var chart = new Chart(ctx).Doughnut(data);
-  });
 });
